6728fad44a04e8fdc73d2eb1bb0056e1b42f4b26
Gajendra Singh Bisalwat <bislawatgolu@gmail.com>
2026-04-17 21:42:36 +0530

    Add files via upload

--- studentPractice.sql
+++ studentPractice.sql
@@ -1,1 +1,13 @@
-show database; 
+create database if not exists student;
+use student;
+
+create table studentdatabase(
+	id int primary key,
+    name varchar(50),
+    age int not null
+);
+
+insert into studentdatabase values(1, "sakshi bhati", 20);
+insert into studentdatabase values(2, "Ary bhati", 20); 
+
+show tables;select * from studentdatabase;
